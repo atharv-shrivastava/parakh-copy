4fb9ee441f64fd1b563868bd5faf2715abf63bd3
Atharv shrivastava <shashishrivastava0290@gmail.com>
2026-09-01 20:30:05 +0530

    Fix product, category and registration routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,15 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import Layout from "./components/Layout";
-
 import Dashboard from "./pages/Dashboard";
 import Scan from "./pages/Scan";
-
 import Shops from "./pages/Shops";
 import ShopDetails from "./pages/ShopDetails";
 import ShopProducts from "./pages/ShopProducts";
-
 import Products from "./pages/Products";
 import ProductDetails from "./pages/ProductDetails";
 import CategoryPage from "./pages/CategoryPage";
 import FoodProductRegistration from "./pages/FoodProductRegistration";
-
 import Food from "./pages/Food";
-
 import History from "./pages/History";
 import Reports from "./pages/Reports";
 import Profile from "./pages/Profile";
@@ -26,55 +19,24 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
-
-          {/* Dashboard */}
           <Route path="/" element={<Dashboard />} />
-
-          {/* Scan */}
           <Route path="/scan" element={<Scan />} />
-
-          {/* Shops */}
           <Route path="/shops" element={<Shops />} />
+          <Route path="/shops/:shopId" element={<ShopDetails />} />
+          <Route path="/shops/:shopId/products" element={<ShopProducts />} />
 
-          <Route
-            path="/shops/:shopId"
-            element={<ShopDetails />}
-          />
-
-          <Route
-            path="/shops/:shopId/products"
-            element={<ShopProducts />}
-          />
-
-          {/* Products */}
           <Route path="/products" element={<Products />} />
-
-          {/* Food */}
           <Route path="/products/food" element={<Food />} />
 
-          {/* Register New Food Product */}
-          <Route
-            path="/products/register/food"
-            element={<FoodProductRegistration />}
-          />
+          <Route path="/products/register" element={<FoodProductRegistration />} />
+          <Route path="/products/register/food" element={<FoodProductRegistration />} />
 
-          {/* Food Subcategories / Product Types */}
-          <Route
-            path="/products/food/:categorySlug"
-            element={<CategoryPage />}
-          />
+          <Route path="/products/category/:categoryId" element={<CategoryPage />} />
+          <Route path="/products/item/:id" element={<ProductDetails />} />
 
-          {/* Individual Products */}
-          <Route
-            path="/products/item/:id"
-            element={<ProductDetails />}
-          />
-
-          {/* Other Pages */}
           <Route path="/history" element={<History />} />
           <Route path="/reports" element={<Reports />} />
           <Route path="/profile" element={<Profile />} />
-
         </Route>
       </Routes>
     </BrowserRouter>
