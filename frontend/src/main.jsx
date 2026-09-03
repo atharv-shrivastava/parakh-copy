06ba8f96031828c4540fab9a70825ded4b6d84d8
Atharv shrivastava <shashishrivastava0290@gmail.com>
2026-09-03 07:50:02 +0530

    fix: remove duplicate theme imports

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,9 +3,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import "./styles/components.css";
-import { applyTheme, getTheme } from "./lib/theme";
-
-applyTheme(getTheme());
 import "./styles/theme.css";
 import { applyTheme, getTheme } from "./lib/theme";
 
