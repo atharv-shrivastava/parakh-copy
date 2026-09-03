43af555400c05d4efc56e9a3d2b2be61a191da13
Atharv shrivastava <shashishrivastava0290@gmail.com>
2026-09-03 07:39:27 +0530

    fix: apply saved theme on app startup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,9 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./styles/global.css";
 import "./styles/components.css";
+import { applyTheme, getTheme } from "./lib/theme";
+
+applyTheme(getTheme());
 import "./styles/theme.css";
 import { applyTheme, getTheme } from "./lib/theme";
 
