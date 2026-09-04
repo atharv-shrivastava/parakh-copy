80114ea019d084f1c9a59df925ecc241db236e17
Atharv shrivastava <shashishrivastava0290@gmail.com>
2026-09-04 14:52:48 +0530

    style: load responsive mobile layout overrides

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,7 @@ import App from "./App";
 import "./styles/global.css";
 import "./styles/components.css";
 import "./styles/theme.css";
+import "./styles/responsive.css";
 import { applyTheme, getTheme } from "./lib/theme";
 
 applyTheme(getTheme());
