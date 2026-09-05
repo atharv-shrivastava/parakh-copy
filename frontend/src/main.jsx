2af171377b3908a2eabe6e3987c05a60f967000d
Atharv shrivastava <shashishrivastava0290@gmail.com>
2026-09-05 22:27:35 +0530

    ui: load ambient theme backgrounds

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,6 +12,7 @@ import "./styles/modern-system.css";
 import "./styles/dark-contrast.css";
 import "./styles/dashboard-polish.css";
 import "./styles/visual-polish.css";
+import "./styles/theme-backgrounds.css";
 import "./styles/rainbow-theme.css";
 import "./styles/dark-contrast-final.css";
 import { applyTheme, getTheme } from "./lib/theme";
