5363a199215268c57c47474a9f2ea7fb5edf1590
Atharv shrivastava <shashishrivastava0290@gmail.com>
2026-09-05 22:38:51 +0530

    feat: load visual motion polish

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -15,6 +15,7 @@ import "./styles/visual-polish.css";
 import "./styles/theme-backgrounds.css";
 import "./styles/rainbow-theme.css";
 import "./styles/dark-contrast-final.css";
+import "./styles/motion-polish.css";
 import { applyTheme, getTheme } from "./lib/theme";
 import { LanguageProvider } from "./components/LanguageProvider";
 
